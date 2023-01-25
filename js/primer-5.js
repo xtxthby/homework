995698d7989f3cc6f9328ce44244e008c606165b
Yurii <116266510+xtxthby@users.noreply.github.com>
2023-01-25 17:34:15 +0200

    add zmina

--- js/primer-5.js
+++ js/primer-5.js
@@ -429,25 +429,25 @@
 
 // console.log(getFriendsByOnlinesStatus(friends));
 
-const products = [
-  { name: "Radar", price: 1300, quantity: 4 },
-  { name: "Scanner", price: 2700, quantity: 3 },
-  { name: "Droid", price: 400, quantity: 7 },
-  { name: "Grip", price: 1200, quantity: 9 },
-];
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
 
-function getAllPropValues(propName) {
-  // Change code below this line
-  const velue = [];
-  for (const product of products) {
-    if(product.name === propName) 
-     velue.push(product); 
-  }
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   const velue = [];
+//   for (const product of products) {
+//     if(product.name === propName) 
+//      velue.push(product); 
+//   }
 
-  return velue;
-  // Change code above this line
-}
-console.log(getAllPropValues("name"));
+//   return velue;
+//   // Change code above this line
+// }
+// console.log(getAllPropValues("name"));
 
 
 // function multiply(firstNumber, secondNumber, ...otherArgs) {
--- js/primer-6.js
+++ js/primer-6.js
@@ -1,21 +1,21 @@
-const cart = {
-    items: [],
-    getItems() {},
-    add(product) {},
-    remove(productName) {},
-    clear() {},
-    countTotalPrice() {},
-    increseQuantity(productName) { },
-    decreaseQuantity(productName) {},
-}
+// const cart = {
+//     items: [],
+//     getItems() {},
+//     add(product) {},
+//     remove(productName) {},
+//     clear() {},
+//     countTotalPrice() {},
+//     increseQuantity(productName) { },
+//     decreaseQuantity(productName) {},
+// }
 
-console.table(cart.getItems());
+// console.table(cart.getItems());
 
-cart.add({ name: "", price: 50 });
-cart.add({ name: "", price: 50 });
-cart.add({ name: "", price: 50 });
-cart.add({ name: "", price: 50 });
+// cart.add({ name: "", price: 50 });
+// cart.add({ name: "", price: 50 });
+// cart.add({ name: "", price: 50 });
+// cart.add({ name: "", price: 50 });
 
-console.table(cart.getItems());
+// console.table(cart.getItems());
 
 
